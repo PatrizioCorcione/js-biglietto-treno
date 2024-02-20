b0fa2df7b1df0d95c9dc2771111d7456a5db9e16
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-02-20 13:44:09 +0100

    calcolo prezzo base

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const eta = prompt('Scrivi la tua eta');
+
+console.log(eta);
+
+const km = prompt('Scrivi i kilometri da percorrere');
+
+console.log(km);
+
+const prezzoKm = 0.21;
+
+let prezzoBigl = km * prezzoKm;
+
+console.log(prezzoBigl);
