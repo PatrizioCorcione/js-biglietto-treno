38f7491ec661a267264d33ee16542a769a7f0df3
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-02-20 15:00:42 +0100

    finezze estetiche

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
 const eta = prompt('Scrivi la tua eta');
 
-console.log(eta);
-
 const km = prompt('Scrivi i kilometri da percorrere');
-
-console.log(km);
 
 const prezzoKm = 0.21;
 
 let prezzoBigl = km * prezzoKm;
 
-console.log(prezzoBigl);
-
 if(eta < 18){
      sconto = (prezzoBigl * 20) / 100;
      prezzoBigl = prezzoBigl - sconto;
-     console.log(prezzoBigl.toFixed(2)); 
+     document.getElementById("conto").innerHTML =
+     `
+     Il tuo conto con sconto del 20% e ${prezzoBigl.toFixed(2)} €
+     `
    } else if(eta > 65){
      sconto = (prezzoBigl * 40) / 100;
      prezzoBigl = prezzoBigl - sconto;
-     console.log(prezzoBigl.toFixed(2));
+     document.getElementById("conto").innerHTML =
+     `
+     Il tuo conto con sconto del 40% e ${prezzoBigl.toFixed(2)} €
+     `
    }else{
-     console.log(prezzoBigl.toFixed(2));
+     document.getElementById("conto").innerHTML =
+     `
+     Il tuo conto senza sconti e ${prezzoBigl.toFixed(2)} €
+     `
    }
 
